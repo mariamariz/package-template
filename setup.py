4311bfdae430c2def378fdef20b92f574716a7e2
mariamariz <mariamariz@gmail.com>
2024-10-21 14:36:03 -0300

    Configuração do pacote ImagemPlus

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="ImagemPlus",
     version="0.0.1",
-    author="my_name",
+    author="Maria Mariz",
     author_email="my_email",
-    description="My short description",
+    description="Criar um programa que aplique filtros básicos em imagens, como preto e branco, sépia ou ajuste de brilho e contraste dando beleza e simplicidade.",
     long_description=page_description,
-    long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    long_description_content_type="ImagemPlus é um pacote Python projetado para aplicar filtros básicos de imagem de forma simples e eficiente. Com ImagemPlus, você pode transformar suas fotos com filtros clássicos como branco e preto, sépia, azul, entre outros. Ideal para quem busca uma maneira rápida e prática de adicionar um toque especial às suas imagens,mas tendo a sutileza da simplicidade.",
+    url="my_github_repository_project_link",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
